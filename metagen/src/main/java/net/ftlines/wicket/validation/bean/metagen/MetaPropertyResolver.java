afe0d2e1d588f539fd8487cc0e1afa749c66b19c
Igor Vaynberg <igor.vaynberg@gmail.com>
2011-07-13 23:27:30 -0700

    code formatting in metagen module

--- metagen/src/main/java/net/ftlines/wicket/validation/bean/metagen/MetaPropertyResolver.java
+++ metagen/src/main/java/net/ftlines/wicket/validation/bean/metagen/MetaPropertyResolver.java
@@ -1,15 +1,13 @@
 /**
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
  */
 
 package net.ftlines.wicket.validation.bean.metagen;
@@ -23,12 +21,15 @@ import net.ftlines.wicket.validation.bean.IPropertyResolver;
  * 
  * @author igor
  */
-public class MetaPropertyResolver implements IPropertyResolver {
+public class MetaPropertyResolver implements IPropertyResolver
+{
 
 	@Override
-	public IProperty resolve(Object object) {
-		if (object instanceof Property) {
-			return new MetaProperty((Property<?, ?>) object);
+	public IProperty resolve(Object object)
+	{
+		if (object instanceof Property)
+		{
+			return new MetaProperty((Property<?, ?>)object);
 		}
 		return null;
 	}
